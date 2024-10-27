8563f46f3b44958caef81ceb77279bba34144111
Big-Leu <abhishek.rawat21b@iiitg.ac.in>
2024-10-28 03:30:51 +0530

    it's working now

--- go_project/go.mod
+++ go_project/go.mod
@@ -4,6 +4,7 @@ go 1.23.1
 
 require (
 	github.com/gin-gonic/gin v1.10.0
+	k8s.io/api v0.31.2
 	k8s.io/apimachinery v0.31.2
 	k8s.io/client-go v0.31.2
 )
@@ -32,6 +33,7 @@ require (
 	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/imdario/mergo v0.3.6 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
@@ -39,9 +41,11 @@ require (
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/moby/spdystream v0.4.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/mxk/go-flowrate v0.0.0-20140419014527-cca7078d478f // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
@@ -59,7 +63,6 @@ require (
 	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	k8s.io/api v0.31.2 // indirect
 	k8s.io/klog/v2 v2.130.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20240228011516-70dd3763d340 // indirect
 	k8s.io/utils v0.0.0-20240711033017-18e509b52bc8 // indirect
--- go_project/main.go
+++ go_project/main.go
@@ -1,20 +1,32 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
-	// v1 "k8s.io/client-go/applyconfigurations/core/v1"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/remotecommand"
 )
+
+type Command struct {
+    PodName string `json:"podname"`
+    Namespace string `json:"namespace"`
+    ContainerName string `json:"containername"`
+    EndPoint string `json:"endpoint"`
+    FunctionName string `json:"functionname"`
+    Route string `json:"route"`
+    FunctionBody string `json:"functionbody"`
+}
 
 type PodInfo struct {
     Name       string        `json:"name"`
@@ -35,7 +47,23 @@ type PodCreateSchema struct {
 	Image string `json:"image"`
 }
 
-func creatClient() *kubernetes.Clientset{
+
+type LogStreamer struct{
+    b bytes.Buffer
+}
+
+func (l *LogStreamer) String() string {
+    return l.b.String()
+}
+
+func (l *LogStreamer) Write(p []byte) (n int, err error) {
+    a := strings.TrimSpace(string(p))
+    l.b.WriteString(a)
+    return len(p), nil
+}
+
+
+func creatClient() (*kubernetes.Clientset,*rest.Config){
   home,_ := os.UserHomeDir()
   kubeConfigPath := filepath.Join(home,".kube/config")
  
@@ -47,11 +75,11 @@ func creatClient() *kubernetes.Clientset{
 
   client:= kubernetes.NewForConfigOrDie(config)
 
-  return client
+  return client,config
 }
 
 func createPods(c *gin.Context){
-	client := creatClient()
+	client,_ := creatClient()
      
      var pod PodCreateSchema
     if err := c.ShouldBindJSON(&pod); err != nil {
@@ -84,8 +112,60 @@ func createPods(c *gin.Context){
 
 }
 
+
+
+func execCommandInPod(c *gin.Context) {
+    client,config := creatClient()
+    var pod Command
+    if err := c.ShouldBindJSON(&pod); err != nil {
+        c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+        return
+    }
+    pythonCommand := fmt.Sprintf(`python script_to.py --endpoint_type %s --function_name %s --route %s --function_file %s`, pod.EndPoint,
+    pod.FunctionName, pod.Route, pod.FunctionBody)
+
+    fmt.Print(pythonCommand)
+    command := []string{"/bin/sh", "-c",pythonCommand}
+
+    // Create the request
+    req := client.CoreV1().RESTClient().
+        Post().
+        Resource("pods").
+        Name(pod.PodName).
+        Namespace(pod.Namespace).
+        SubResource("exec").
+        Param("container",pod.ContainerName).
+        Param("stdout", "true").
+        Param("stderr", "true").
+        Param("tty", "false")
+
+    // Add command parameters correctly
+    for _, cmd := range command {
+        req.Param("command", cmd)
+    }
+    l := &LogStreamer{}
+    Executor,err := remotecommand.NewSPDYExecutor(config, http.MethodPost, req.URL())
+
+    Executor.StreamWithContext(context.Background(),remotecommand.StreamOptions{
+        Stdin:  os.Stdin,
+        Stdout: l,
+        Stderr: nil,
+        Tty:    true,
+    })
+
+    if err != nil {
+        c.String(http.StatusInternalServerError, "Error executing command: %s", err.Error())
+        return
+    }
+
+    c.String(http.StatusOK, "Command executed successfully")
+}
+
+
+
+
 func getPods(c *gin.Context){
-    client := creatClient()
+    client,_:= creatClient()
 	pods,err :=client.CoreV1().Pods("default").List(context.Background(),metav1.ListOptions{})
 
 	if err != nil {
@@ -122,5 +202,6 @@ func main() {
   router := gin.Default()
   router.GET("/api/v1/getpods",getPods)
   router.POST("/api/v1/createpods",createPods)
+  router.POST("/api/v1/add",execCommandInPod)
   router.Run("localhost:8080")
 }
